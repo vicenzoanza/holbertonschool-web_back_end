d58fd3052ecf8be99aa2f10f4bb02cb2bc17eafd
vicenzoanza <viceanza2010@hotmail.com>
2023-08-30 06:40:00 -0700

    tasks

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" task 5 """
+
+
+def sum_list(input_list : float) -> float:
+    """ returns their sum as a float """
+    return sum(input_list)
---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" task 6 """
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst : Union[int, float]) -> float:
+    """ integers and floats and returns their sum as a float """
+    return sum(mxd_lst)
