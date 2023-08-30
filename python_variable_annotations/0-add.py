3074e1c6291d8a5e39d6cf00cac9befee9ff6fab
vicenzoanza <viceanza2010@hotmail.com>
2023-08-30 00:21:52 -0700

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
-    """takes a float a and a float b as arguments and returns their sum as a float."""
+    """take float a and a float b and returns their sum as a float."""
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def concat(str1: str, str2: str) -> str:
-    """ takes a string str1 and a string str2 as arguments and returns a concatenated string """
+    """takes a string str1 and a string str2, returns a concatenated string """
     return str1 + str2
