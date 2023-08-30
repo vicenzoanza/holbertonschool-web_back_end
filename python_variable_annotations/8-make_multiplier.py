a35cf6d1bad5b6ec259c1e75637aebf21fca8f1c
vicenzoanza <viceanza2010@hotmail.com>
2023-08-30 07:14:54 -0700

    task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,9 +3,9 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) ->  Callable[[float], float]:
-    """ """
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ make multiplier"""
     def multiplier_function(x: float) -> float:
-        """ """
+        """ multiplier """
         return x * multiplier
     return multiplier_function
