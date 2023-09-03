9205693f6fc7630cf128b5e77eae9915ca19f475
root <viceanza2010@hotmail.com>
2023-09-03 15:48:36 -0700

    task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" task 0 """
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """ generate delay 0 to 10 """
+    delay = random.uniform(0, max_delay)
+    
+    await asyncio.sleep(delay)
+    
+    return delay
---
+++ python_async_function/README.md
@@ -0,0 +1,1 @@
+dasdaddds
