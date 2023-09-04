195f5216d543e725074e4442e5198bce5426309a
root <viceanza2010@hotmail.com>
2023-09-03 19:21:52 -0700

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" task 0 """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Write a coroutine that takes no arguments."""
+    random = random.uniform(0, max_delay)
+    await asyncio.sleep(random)
+    return random
