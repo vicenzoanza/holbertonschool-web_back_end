c13d85596dfc8197f87a5349950f2ac39005181c
root <viceanza2010@hotmail.com>
2023-09-03 17:09:59 -0700

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ should return the list of all the delays (float values). """
     delays = []
 
