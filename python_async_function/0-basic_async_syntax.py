ca6d600b56278992b6a93daeb93c510b00db829e
root <viceanza2010@hotmail.com>
2023-09-03 17:06:01 -0700

    task 1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """ generate delay 0 to 10 """
     delay = random.uniform(0, max_delay)
-    
+
     await asyncio.sleep(delay)
-    
+
     return delay
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" task 1 """
+import asyncio
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """ should return the list of all the delays (float values). """
+    delays = []
+
+    async def wait_n2():
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    await asyncio.gather(*[wait_n2() for _ in range(n)])
+
+    delays.sort()
+
+    return delays
